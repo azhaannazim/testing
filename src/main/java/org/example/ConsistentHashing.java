f2d42e13597fa79b8fc2df8ac9892642af63fe4b
Azhaan Nazim <nazimazhaan17@gmail.com>
2025-12-19 22:38:20 +0530

    initial

---
+++ src/main/java/org/example/ConsistentHashing.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class ConsistentHashing {
+    private final int numReplicas;
+    private final TreeMap<Long , String> ring;
+    private final Set<String> servers;
+
+    public ConsistentHashing(List<String> servers ,int numReplicas){
+        this.numReplicas = numReplicas;
+        this.servers = new HashSet<>();
+        this.ring = new TreeMap<>();
+
+        for(String server : servers){
+            addServer(server);
+        }
+    }
+    public long hash(String key) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(key.getBytes());
+            byte[] digest = md.digest();
+            return ((long) (digest[0] & 0xFF) << 24) |
+                    ((long) (digest[1] & 0xFF) << 16) |
+                    ((long) (digest[2] & 0xFF) << 8) |
+                    ((long) (digest[3] & 0xFF));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 algorithm not found", e);
+        }
+    }
+
+    public void addServer(String server) {
+        servers.add(server);
+        for(int i=0;i<numReplicas;i++){
+            long hash = hash(server + "-" + i);
+            ring.put(hash, server);
+        }
+    }
+
+    public String getServer(String key){
+        if(ring.isEmpty()) return null;
+
+        long hash = hash(key);
+        Map.Entry<Long, String> entry = ring.ceilingEntry(hash);
+
+        if(entry == null){
+            entry = ring.firstEntry();
+        }
+        return entry.getValue();
+    }
+
+    public void removeServer(String server) {
+        if (servers.remove(server)) {
+            for (int i = 0; i < numReplicas; i++) {
+                long hash = hash(server + "-" + i);
+                ring.remove(hash);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        List<String> servers = Arrays.asList("S0", "S1", "S2", "S3", "S4", "S5");
+        ConsistentHashing ch = new ConsistentHashing(servers, 3);
+
+        // Step 2: Assign requests (keys) to servers
+        System.out.println("UserA is assigned to: " + ch.getServer("User A"));
+        System.out.println("UserB is assigned to: " + ch.getServer("UserB"));
+
+        // Step 3: Add a new server dynamically
+        ch.addServer("S6");
+        System.out.println("UserA is now assigned to: " + ch.getServer("UserA"));
+
+        // Step 4: Remove a server dynamically
+        ch.removeServer("S2");
+        System.out.println("UserB is now assigned to: " + ch.getServer("UserB"));
+
+    }
+}
